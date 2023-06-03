fd9deb580485c9a1d73ec81b10f4096bd1c11055
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-02 22:27:21 -0400

    Boilerplate for Spring

---
+++ Expensify/src/main/java/com/example/Expensify/ExpensifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Expensify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensifyApplication.class, args);
+	}
+
+}
