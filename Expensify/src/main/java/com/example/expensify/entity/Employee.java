a604aec1807750ebada4b6d2e2227531e2370217
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-08 04:34:38 -0400

    Initialize the expenses array and add anotation for enum type to string

--- Expensify/src/main/java/com/example/expensify/entity/Employee.java
+++ Expensify/src/main/java/com/example/expensify/entity/Employee.java
@@ -2,6 +2,7 @@ package com.example.expensify.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,20 +13,22 @@ public class Employee extends ExpensifyUser {
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   @JoinTable(
       name = "employees_expenses",
-      joinColumns = {@JoinColumn(name = "employee_id")},
-      inverseJoinColumns = {@JoinColumn(name = "expense_id")})
+      joinColumns = {@JoinColumn()},
+      inverseJoinColumns = {@JoinColumn()})
   private List<Expense> expenses;
 
   private String username;
 
   private String password;
 
+  @Enumerated(EnumType.STRING)
   private Role role;
 
   protected Employee() {}
 
   public Employee(String username, String password, Role role) {
     super(username, password, role);
+    this.expenses = new ArrayList<>();
     this.username = username;
     this.password = password;
     this.role = role;
