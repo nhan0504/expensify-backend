2742a5487d29d7409606708dc1a51a5ce15a0e2f
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-10 05:15:47 -0400

    Get all expenses for an employee

---
+++ Expensify/src/main/java/com/example/expensify/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.example.expensify.controller;
+
+import com.example.expensify.entity.Employee;
+import com.example.expensify.entity.Expense;
+import com.example.expensify.repository.EmployeeRepository;
+import com.example.expensify.repository.ExpenseRepository;
+import com.example.expensify.repository.StatusRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+  private final EmployeeRepository employeeRepository;
+  private final ExpenseRepository expenseRepository;
+  private final StatusRepository statusRepository;
+
+  EmployeeController(
+      EmployeeRepository employeeRepository,
+      ExpenseRepository expenseRepository,
+      StatusRepository statusRepository) {
+    this.employeeRepository = employeeRepository;
+    this.expenseRepository = expenseRepository;
+    this.statusRepository = statusRepository;
+  }
+
+  @GetMapping("/employees/{employee_id}/expenses")
+  public List<Expense> get_employee_expenses(@PathVariable Long employee_id) {
+    return employeeRepository.findById(employee_id).orElseThrow().getExpenses();
+  }
+
+}
