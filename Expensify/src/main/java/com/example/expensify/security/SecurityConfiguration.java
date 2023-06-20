1b6bb33a7e758ef4cdf492bb5b8d4835961fc526
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-20 04:12:02 -0400

    Remove matching with HtppMethod

--- Expensify/src/main/java/com/example/expensify/security/SecurityConfiguration.java
+++ Expensify/src/main/java/com/example/expensify/security/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.example.expensify.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,11 +23,11 @@ public class SecurityConfiguration {
         .authorizeHttpRequests(
             authorize ->
                 authorize
-                    .requestMatchers(HttpMethod.GET, "/employees")
+                    .requestMatchers("/employees")
                     .hasRole("REVIEWER")
-                    .requestMatchers(HttpMethod.PUT, "/expenses/{expenseId}/status")
+                    .requestMatchers("/expenses/{expenseId}/status")
                     .hasRole("REVIEWER")
-                    .requestMatchers("/employees/{employeeId}/expenses")
+                    .requestMatchers("/employees/{employeeId}/expenses/**")
                     .hasRole("EMPLOYEE")
                     .anyRequest()
                     .authenticated())
