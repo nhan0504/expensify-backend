50e7dd2f5c780d3fa53aab37a80c4fdd3b454db9
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-25 23:21:35 +0700

    Add cor and response

--- Expensify/src/main/java/com/example/expensify/security/SecurityConfiguration.java
+++ Expensify/src/main/java/com/example/expensify/security/SecurityConfiguration.java
@@ -8,8 +8,22 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+<<<<<<< Updated upstream
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+=======
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+>>>>>>> Stashed changes
 
 @EnableWebSecurity
 @Configuration
@@ -23,7 +37,12 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain web(HttpSecurity http) throws Exception {
+<<<<<<< Updated upstream
     http.csrf(csrf -> csrf.disable())
+=======
+    http.cors(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable())
+>>>>>>> Stashed changes
         .authorizeHttpRequests(
             authorize ->
                 authorize
@@ -41,6 +60,21 @@ public class SecurityConfiguration {
                 form.successHandler(
                         (request, response, authentication) -> {
                           response.setStatus(HttpServletResponse.SC_OK);
+<<<<<<< Updated upstream
+=======
+                          response.setContentType("application/json");
+
+                          UserDetails principal = (UserDetails) authentication.getPrincipal();
+                          String principalDetails =
+                              String.format(
+                                  "{\"username\": \"%s\", \"authorities\": \"%s\"}",
+                                    principal.getUsername(),
+                                    principal.getAuthorities().toArray()[0].toString());
+
+                          PrintWriter out = response.getWriter();
+                          out.print(principalDetails);
+                          out.flush();
+>>>>>>> Stashed changes
                         })
                     .failureHandler(
                         (request, response, exception) -> {
@@ -48,4 +82,17 @@ public class SecurityConfiguration {
                         }));
     return http.build();
   }
+<<<<<<< Updated upstream
+=======
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+>>>>>>> Stashed changes
 }
