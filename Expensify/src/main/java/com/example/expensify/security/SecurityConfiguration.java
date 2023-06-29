d7aa9b5998365aab7c0f1cf1ceb940ffb5dca71b
nhan0504 <76593076+nhan0504@users.noreply.github.com>
2023-06-29 10:11:49 +0700

    Form login (#14)
    Enable CORS and return user detial in server response

--- Expensify/src/main/java/com/example/expensify/security/SecurityConfiguration.java
+++ Expensify/src/main/java/com/example/expensify/security/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.expensify.security;
 
+import com.example.expensify.entity.ExpensifyUser;
 import com.example.expensify.entity.Role;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,10 +14,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 @EnableWebSecurity
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+  @Autowired ObjectMapper objectMapper;
 
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
@@ -24,6 +34,7 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain web(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests(
             authorize ->
                 authorize
@@ -41,11 +52,32 @@ public class SecurityConfiguration {
                 form.successHandler(
                         (request, response, authentication) -> {
                           response.setStatus(HttpServletResponse.SC_OK);
+                          response.setContentType("application/json");
+
+                          ExpensifyUser expensifyUser =
+                              (ExpensifyUser) authentication.getPrincipal();
+                          response
+                              .getWriter()
+                              .write(
+                                  objectMapper
+                                      .writerFor(ExpensifyUser.class)
+                                      .writeValueAsString(expensifyUser));
                         })
                     .failureHandler(
                         (request, response, exception) -> {
                           response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                         }));
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
